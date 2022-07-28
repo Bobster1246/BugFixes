636e236b9974e05cf8fd4a3e258b5a10ed0bc508
Bobster1246 <bobster1246@gmail.com>
2022-07-27 17:21:27 -0700

    First

---
+++ BugFixes/Main.cs
@@ -0,0 +1,102 @@
+﻿using UnityModManagerNet;
+using Kingmaker.UnitLogic.FactLogic;
+using HarmonyLib;
+using Kingmaker.UnitLogic.Mechanics;
+using Kingmaker.Blueprints.Classes.Spells;
+using Kingmaker.EntitySystem;
+using Kingmaker.EntitySystem.Entities;
+using Kingmaker.EntitySystem.Stats;
+using Kingmaker.UnitLogic.Abilities;
+using System;
+
+
+namespace BugFixes
+{
+    static class Main
+    {
+        public static UnityModManager.ModEntry modEntry;
+
+        static bool Load(UnityModManager.ModEntry mE)
+        {
+            modEntry = mE;
+            var harmony = new Harmony(modEntry.Info.Id);
+            harmony.PatchAll();
+            return true;
+        }
+        
+    }
+
+    //HarmonyPatch attribute allows PatchAll to find the patch
+    [HarmonyPatch(typeof(AddStatBonus), nameof(AddStatBonus.TryApplyArcanistPowerfulChange))]
+    static class AddStatBonus_TryApplyArcanistPowerfulChange_Patch
+    {
+        static bool Prefix(EntityFact fact, StatType stat, int value, ref PowerfulChangeType? powerfulChange, ref int __result)
+        {
+
+            __result = FixedTryApplyArcanistPowerfulChange(fact, stat, value, ref powerfulChange);
+            return false;
+        }
+
+        public static int FixedTryApplyArcanistPowerfulChange(EntityFact fact, StatType stat, int value, ref PowerfulChangeType? powerfulChange)
+        {
+            try
+            {
+                MechanicsContext mechanicsContext = fact?.MaybeContext;
+                if (mechanicsContext == null)
+                {
+                    return value;
+                }
+                UnitEntityData maybeCaster = mechanicsContext.MaybeCaster;
+                if (maybeCaster == null)
+                {
+                    Main.modEntry.Logger.Log("Caster is missing");
+                    return value;
+                }
+                if (!stat.IsAttribute() || value < 0)
+                {
+                    return value;
+                }
+                AbilityExecutionContext sourceAbilityContext = mechanicsContext.SourceAbilityContext;
+                if (!powerfulChange.HasValue)
+                {
+                    if (sourceAbilityContext?.Ability?.Spellbook == null)
+                    {
+                        return value;
+                    }
+                    if (!sourceAbilityContext.Ability.Blueprint.IsSpell)
+                    {
+                        return value;
+                    }
+                    if (!sourceAbilityContext.Ability.Spellbook.Blueprint.IsArcanist)
+                    {
+                        return value;
+                    }
+                    if (sourceAbilityContext.SpellSchool != SpellSchool.Transmutation)
+                    {
+                        return value;
+                    }
+                }
+                if ((bool)maybeCaster.State.Features.ImprovedPowerfulChange || powerfulChange == PowerfulChangeType.Improved)
+                {
+                    powerfulChange = PowerfulChangeType.Improved;
+                    value += 4;
+                }
+                else if ((bool)maybeCaster.State.Features.PowerfulChange || powerfulChange == PowerfulChangeType.Simple)
+                {
+                    powerfulChange = PowerfulChangeType.Simple;
+                    value += 2;
+                }
+                
+                return value;
+            }
+            catch (Exception ex)
+            {
+                Main.modEntry.Logger.LogException(ex);
+                return value;
+            }
+        }
+    }
+
+   
+ 
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # BugFixes
+
+BugFixes is a simple mod dedicated to fixing small bugs in Pathfinder: Wrath of the Righteous.
+
+This mod must be installed with the [Unity Mod Manager](https://www.nexusmods.com/site/mods/21?tab=posts)
+
+See the releases tab for the latest release.
+
+Please file issues (with an attached save if possible) for suggested additional fixes.
+
+Currently implemented fixes:
+
+- Brownfur Transmuter capstone now properly applies +4 to each buff 
